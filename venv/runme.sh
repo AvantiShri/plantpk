fa97d357299bb65a3c4ea06492f5d6a8eaed2acf
Avanti Shrikumar <ashrikumar@noetic.org>
2026-02-22 12:36:33 +0530

    first commit

---
+++ venv/runme.sh
@@ -0,0 +1,5 @@
+#Setup:
+python3 -m venv truerng-env
+source truerng-env/bin/activate
+#List from https://github.com/euler357/TrueRNG/blob/master/README.md#linux-instructions-ubuntu--debian-based
+python3 -m pip install pyserial pyusb matplotlib numpy nltk 
